66ab59336eb37f908419bbfdc495d12b3bc7b20d
kunal982006 <kunal982006@gmail.com>
2026-02-20 14:57:53 +0530

    Updated code from Antigravity

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shirurexpress.app',
+  appName: 'Shirur Express',
+  webDir: 'dist/public'
+};
+export default config;
