47b9b9c2d2e5f3b13495b128383eeb080340373a
Kunal Shinde <kunal982006@gmail.com>
2026-07-05 19:41:52 +0530

    push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router as LocationProvider } from "wouter";
+import { Switch, Route, Router as LocationProvider, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
